7510b0ca508b846af53f25afa1cb9816fcfcdba1
detorhu <rajmanhotra6969@gmail.com>
2026-01-31 22:24:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION = os.getenv("SESSION")  # string ya file-based session
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION")
-
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-
-OWNER_ID = int(os.getenv("OWNER_ID"))
+MONGO_URI = os.getenv("MONGO_URI")  # optional
