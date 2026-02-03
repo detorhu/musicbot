291d3feb370d62bcf9973f25fd61ad96fb133ab2
detorhu <rajmanhotra6969@gmail.com>
2026-02-03 10:35:25 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")  # string ya file-based session
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-
-MONGO_URI = os.getenv("MONGO_URI")  # optional
