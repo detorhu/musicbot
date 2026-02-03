8855c246d441f002854aa75fe1d1c34606c8e9ac
detorhu <rajmanhotra6969@gmail.com>
2026-02-03 10:52:02 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -1,19 +1,26 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
+
 from bot.config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
 
+
+# Telegram Bot
 bot = Client(
-    "MusicBot",
+    name="MusicBot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=BOT_TOKEN,
+    in_memory=True
 )
 
+# Assistant Account (VC join karega)
 assistant = Client(
-    "Assistant",
+    name="Assistant",
     api_id=API_ID,
     api_hash=API_HASH,
-    session_string=SESSION_STRING
+    session_string=SESSION_STRING,
+    in_memory=True
 )
 
+# PyTgCalls instance
 call = PyTgCalls(assistant)
